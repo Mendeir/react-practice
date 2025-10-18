60fdf4e4cb7f3f7c13597d143cf8deb9c387ad64
Adrianne Magracia <adriannemagracia.git@gmail.com>
2025-10-18 20:20:02 +0800

    feat: add simple header, menu, and footer

--- pizza-menu/src/App.tsx
+++ pizza-menu/src/App.tsx
@@ -1,5 +1,37 @@
 function App() {
-  return <h1>Pizza Menu</h1>;
+  return (
+    <div>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>MG Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our Menu</h2>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>{new Date().toLocaleTimeString()}. We're currently open!</footer>
+  );
+}
+
+function Pizza() {
+  return <h2>Pizza</h2>;
 }
 
 export default App;
