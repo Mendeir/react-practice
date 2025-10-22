dd9344c2b7e5a42b32ccbc94b54bc4eeddc57614
Adrianne Magracia <adriannemagracia.git@gmail.com>
2025-10-22 22:30:50 +0800

    refactor: extract order functionality into its own component

--- pizza-menu/src/App.tsx
+++ pizza-menu/src/App.tsx
@@ -60,19 +60,24 @@ function Footer() {
   return (
     <footer className="footer">
       {isOpen ? (
-        <div className="order">
-          <p>
-            We're open until {closeHour}:00. Comie visit us or order online!
-            Time is {hour}
-          </p>
-          <button className="btn">Order</button>
-        </div>
+        <Order closeHour={closeHour} />
       ) : (
         <p>
           We're happy to welcome you between {openHour}:00 and {closeHour}:00.
         </p>
       )}
     </footer>
+  );
+}
+
+function Order(props: { closeHour: number }) {
+  return (
+    <div className="order">
+      <p>
+        We're open until {props.closeHour}:00. Come visit us or order online!
+      </p>
+      <button className="btn">Order</button>
+    </div>
   );
 }
 
